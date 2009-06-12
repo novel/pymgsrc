d4ce4c5a824d44b56bae110562eb33a4a4dfed9c
Roman Bogorodskiy <bogorodskiy@gmail.com>
2009-06-12 23:03:12 +0400

    Add some metainfo to setup.py.

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
         version = "0.1.2",
         author = "Roman Bogorodskiy",
         author_email = "novel@FreeBSD.org",
+        url = "http://github.com/novel/pymgsrc/tree/master",
+        description  = "Command line client for imgsrc.ru in Python",
+        long_description = "Command line client for imgsrc.ru photo hosting written in Python.",
+        license = "BSD",
         packages = ["imgsrc"],
         scripts = ["pymgsrc.py"],
 )
